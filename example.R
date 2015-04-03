04b13278d399121e43ea255b1a17cc344ead4a69
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2015-04-03 17:31:08 +0200

    libs folder example images

--- example.R
+++ example.R
@@ -2,13 +2,13 @@
 # HCOMP 15 - Micro task time execution
 #####################################################################
 
-source("crowdResultsCollect.R")
-source("crowdResultsPlot.R")
+source("libs/crowdResultsCollect.R")
+source("libs/crowdResultsPlot.R")
 
 # collect results data
 mturk <- collectResults("MTURK", 1884619,"Transcribe Receipts", "batch1", F)
 crowdflower <- collectResults("CrowdFlower",710063,"Transcribe Receipts", "batch1", F)
 
 # generate plots for the results and save them in files
-plotRectangles(mturk,"example_mturk.pdf", 20,15)
-plotRectangles(crowdflower,"example_crowdflower.pdf", 20,15)
+plotRectangles(mturk,"plots/example_mturk.pdf", 20,15)
+plotRectangles(crowdflower,"plots/example_crowdflower.pdf", 20,15)
